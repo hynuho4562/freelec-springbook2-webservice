ae033543ffa2c2642526dc9738de773f87c4b14b
hyunho4562 <kyounguk192@gmail.com>
2023-03-06 22:34:20 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹 서비스 4장 완료

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,17 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+    }
+}
